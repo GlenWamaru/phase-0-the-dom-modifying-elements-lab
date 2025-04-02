959bb9296b5dbfaaf00efe7e1ede9404b4b30730
Glen Wamaru <garpirungu@gmail.com>
2025-04-02 11:25:08 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const main = document.getElementById("main");
+
+if (main) {
+    main.remove();
+}
+
+const newHeader = document.createElement("h1");
+document.body.appendChild(newHeader);
+
+newHeader.setAttribute("id", "victory");
+
+newHeader.textContent = "Glen is the champion";
